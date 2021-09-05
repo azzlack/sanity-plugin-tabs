3e3f6a2c47297cc8891d8cb53c2238512cc4b8f4
Alexis Hope <alexis@cvofhope.com>
2021-09-06 09:24:47 +1000

    fix: set isRoot to false

--- src/index.js
+++ src/index.js
@@ -288,6 +288,7 @@ class Tabs extends React.Component {
                 focusPath: focusPath,
                 readOnly: fieldReadOnly,
                 value: fieldValue,
+                isRoot: false,
                 onFocus: (path) => this.onFieldFocusHandler(field, path),
                 onChange: (patchEvent) =>
                   this.onFieldChangeHandler(field, patchEvent),
